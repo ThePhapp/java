f73d6e2e9b78ceab31c0b2dfebf68604a03034e5
Thế Pháp <phapok0@gmail.com>
2025-01-08 18:01:42 +0700

    .

---
+++ spring-boot-non-jwt/src/main/java/com/javaweb/customException/FieldRequiredException.java
@@ -0,0 +1,9 @@
+package com.javaweb.customException;
+
+public class FieldRequiredException extends RuntimeException {
+
+	public FieldRequiredException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
